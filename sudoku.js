deda98b0456fd120f4897ee2d716ea1a70d8fc05
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-03-18 10:09:44 +0530

    tracer added.

--- sudoku.js
+++ sudoku.js
@@ -1,32 +1,61 @@
-var screenWidth = 810;
-var screenHeight = 810;
+var canvasWidth = 950;
+var canvasHeight = 450;
+
+var sudokuOrigin = [0, 0]
+var sudokuDim = [canvasHeight, canvasHeight]
+
+var tracerOrigin = [canvasHeight + 50, 0]
+var tracerDim = [canvasHeight, canvasHeight]
+
 var grid = [];
+var solverGen = null;
+var isNextMove = true;
+
+function* solver() {
+    for (var i = 0; i < 9; i++) {
+        var x = yield;
+        grid[0][i] = 'A'
+    }
+}
 
 function setup() {
     grid = sudoku.board_string_to_grid(sudoku.generate(17, true))
-    createCanvas(screenWidth, screenHeight);
+    createCanvas(canvasWidth, canvasHeight);
     background(0, 0, 0);
-    console.log(grid)
+    solverGen = solver()
 }
 
 function _drawLines() {
     stroke(255)
-    for (var i = 1 ; i < 9; i++) {
-        line(0, i * width/9, width, i * width/9)
-        line(i * width/9, 0, i * width/9, width)
+    for (var i = 0 ; i <= 9; i++) {
+        line(sudokuOrigin[0] + 0, sudokuOrigin[1] + i * sudokuDim[1]/9, sudokuOrigin[0] + sudokuDim[0], sudokuOrigin[1] + i * sudokuDim[1]/9)  // horizontal line
+        line(sudokuOrigin[0] + i * sudokuDim[0]/9, sudokuOrigin[1] + 0, sudokuOrigin[0] + i * sudokuDim[0]/9, sudokuOrigin[1] + sudokuDim[1]) // vertical line
+    }
+
+    for (var i = 0 ; i <= 9; i++) {
+        line(tracerOrigin[0] + 0, tracerOrigin[1] + i * tracerDim[1]/9, tracerOrigin[0] + tracerDim[0], tracerOrigin[1] + i * tracerDim[1]/9)  // horizontal line
+        line(tracerOrigin[0] + i * tracerDim[0]/9, tracerOrigin[1] + 0, tracerOrigin[0] + i * tracerDim[0]/9, tracerOrigin[1] + tracerDim[1]) // vertical line
     }
 }
 
 function _drawGrid() {
-    textSize(32);
-    fill(255);
-    stroke(255);
-    for (var i = 0; i < 9; i ++) {
-        for (var j = 0 ; j < 9; j++) {
-            if (grid[i][j] !== ".") {
-                text(grid[i][j], j * width/9 + (width/9)/2 - 32/4, i * width/9 + (width/9)/2 + 32/4);
-            }
-        }
+    // textSize(32);
+    // fill(255);
+    // stroke(255);
+    // for (var i = 0; i < 9; i ++) {
+    //     for (var j = 0 ; j < 9; j++) {
+    //         if (grid[i][j] !== ".") {
+    //             text(grid[i][j], j * width/9 + (width/9)/2 - 32/4, i * width/9 + (width/9)/2 + 32/4);
+    //         }
+    //     }
+    // }
+}
+
+
+function doNextMove() {
+    if (isNextMove) {
+        var x = solverGen.next()
+        isNextMove = !x.done
     }
 }
 
@@ -37,4 +66,5 @@ function _drawBoard() {
 
 function draw() {
     _drawBoard()
+    doNextMove()
 }
