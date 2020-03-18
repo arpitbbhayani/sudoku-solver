89ddfbc23f6980b2b1472c27f77012cfa50026d0
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-03-18 15:12:32 +0530

    cleanin up

--- sudoku.js
+++ sudoku.js
@@ -1,21 +1,19 @@
-var canvasWidth = 950;
+var canvasWidth = 450;
 var canvasHeight = 450;
 
 var sudokuOrigin = [0, 0]
 var sudokuDim = [canvasHeight, canvasHeight]
 
-var tracerOrigin = [canvasHeight + 50, 0]
-var tracerDim = [canvasHeight, canvasHeight]
-
 var grid = [];
+var colorGrid = [];
 var solverGen = null;
 
 function possible(x, y, v) {
     for (var i = 0; i < 9; i++) {
-        if (grid[x][i] === `${v}`) {
+        if (grid[x][i] === v) {
             return false;
         }
-        if (grid[i][y] === `${v}`) {
+        if (grid[i][y] === v) {
             return false;
         }
     }
@@ -23,7 +21,7 @@ function possible(x, y, v) {
     var y0 = int(y/3)*3;
     for (var i = 0; i < 3; i++) {
         for (var j = 0; j < 3; j++) {
-            if (grid[x0 + i][y0 + j] === `${v}`) {
+            if (grid[x0 + i][y0 + j] === v) {
                 return false
             }
         }
@@ -34,15 +32,15 @@ function possible(x, y, v) {
 function* solver() {
     for (var i = 0; i < 9; i++) {
         for (var j = 0; j < 9; j++) {
-            if (grid[i][j] === '.') {
+            if (grid[i][j] === 0) {
                 for (var x = 1; x <= 9; x++) {
                     if (possible(i, j, x)) {
-                        grid[i][j] = `${x}`
+                        grid[i][j] = x
                         yield
                         if (yield* solver()) {
                             return true
                         } else {
-                            grid[i][j] = '.'
+                            grid[i][j] = 0
                             yield
                         }
                     }
@@ -56,10 +54,19 @@ function* solver() {
 
 function setup() {
     solverGen = null
-    grid = sudoku.board_string_to_grid(sudoku.generate(44, true))
+    grid = sudoku.board_string_to_grid(sudoku.generate(20, true))
+    for (var i = 0; i < 9; i++) {
+        var a = new Array(9)
+        for (var j = 0 ; j < 9; j++) {
+            grid[i][j] = grid[i][j] === "." ? 0 : parseInt(grid[i][j], 10)
+            a[j] = grid[i][j] === 0 ? '#FEE715' : 200;
+        }
+        colorGrid.push(a)
+    }
     createCanvas(canvasWidth, canvasHeight);
     background(0, 0, 0);
     solverGen = solver()
+    frameRate(10)
 }
 
 function _drawLines() {
@@ -68,20 +75,16 @@ function _drawLines() {
         line(sudokuOrigin[0] + 0, sudokuOrigin[1] + i * sudokuDim[1]/9, sudokuOrigin[0] + sudokuDim[0], sudokuOrigin[1] + i * sudokuDim[1]/9)  // horizontal line
         line(sudokuOrigin[0] + i * sudokuDim[0]/9, sudokuOrigin[1] + 0, sudokuOrigin[0] + i * sudokuDim[0]/9, sudokuOrigin[1] + sudokuDim[1]) // vertical line
     }
-
-    for (var i = 0 ; i <= 9; i++) {
-        line(tracerOrigin[0] + 0, tracerOrigin[1] + i * tracerDim[1]/9, tracerOrigin[0] + tracerDim[0], tracerOrigin[1] + i * tracerDim[1]/9)  // horizontal line
-        line(tracerOrigin[0] + i * tracerDim[0]/9, tracerOrigin[1] + 0, tracerOrigin[0] + i * tracerDim[0]/9, tracerOrigin[1] + tracerDim[1]) // vertical line
-    }
 }
 
 function _drawValues() {
-    textSize(canvasHeight/20);
-    fill(255);
-    stroke(200);
+    textFont("monospace")
+    textSize(canvasHeight/22);
     for (var i = 0; i < 9; i ++) {
         for (var j = 0 ; j < 9; j++) {
-            if (grid[i][j] !== ".") {
+            if (grid[i][j] !== 0) {
+                fill(colorGrid[i][j]);
+                stroke(colorGrid[i][j]);
                 text(grid[i][j], sudokuOrigin[0] + j * sudokuDim[0]/9 + (sudokuDim[0]/9)/2 - 32/4, sudokuOrigin[1] + i * sudokuDim[0]/9 + (sudokuDim[0]/9)/2 + 32/4);
             }
         }
@@ -89,7 +92,7 @@ function _drawValues() {
 }
 
 function _drawBoard() {
-    background(0)
+    background('#000')
     _drawLines()
     _drawValues()
 }
@@ -98,10 +101,6 @@ function draw() {
     _drawBoard()
     x = solverGen.next()
     if (x.done) {
-        // textSize(64)
-        // fill(0, 255, 0)
-        // stroke(0, 255, 0)
-        // text("Solved!", sudokuOrigin[0] + sudokuDim[0]/2 - 100, sudokuOrigin[1] + sudokuDim[1]/2) - 20;
         noLoop()
     }
 }
